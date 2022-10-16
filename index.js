64898ab81358eb502fdeecc375d0612402c9472e
Yuriko <ygabriela.arnott@gmail.com>
2022-10-16 12:30:00 -0400

    commit tree 2c55f6a49bec8352f0020228be38fc1bded715c5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+//const name = ["Guadalupe", "Ollie", "Aki"];
+//const events = ["surprise"]
+function writeCards(name,events) {
+    let NewArray=[];
+  for (let i = 0; i < name.length; i++) {
+    let message = (`Thank you, ${name[i]}, for the wonderful ${events} gift!`)
+     NewArray.push(message);
+  }
+  return NewArray;
+}
+
+//const int =parseInt(int);
+function countDown(int){
+   // let int = 11;
+    while (int >= 0) {
+      console.log(int);
+      int--;
+    }
+
+}
